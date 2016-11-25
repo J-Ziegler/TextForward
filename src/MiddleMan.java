8cfbda1f3ba9b59a8168d2da1be5c17bb6b8d912
J-Ziegler <ziegl210@morris.umn.edu>
2016-11-25 16:30:12 -0600

    Initial commit of code

---
+++ src/MiddleMan.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by J-Ziegler on 11/24/16.
+ */
+
+public class MiddleMan {
+
+	public static void main(String[] args) {
+        System.out.println("Server Started");
+        ServerSocket servSock = null;
+        try {
+            servSock = new ServerSocket(46163);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Main Loop
+		while(true) {
+            try {
+				Socket client = servSock.accept();
+                System.out.println("Client has connected");
+                ObjectOutputStream outStrm = new ObjectOutputStream(client.getOutputStream());
+                ObjectInputStream inStrm = new ObjectInputStream(client.getInputStream());
+
+				while (!client.isInputShutdown()) {
+                    Status o = (Status) inStrm.readObject();
+                    outStrm.writeObject(o);
+                    outStrm.flush();
+                }
+						
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} // End Main Loop
+	}
+}
